4f6ea229057e2d29265d90ed2f69bfee0b869bb0
Bgpizarro <borjagarpiz@gmail.com>
2023-06-02 14:06:54 +0200

    107

---
+++ 03-gifs-app/src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit{
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  public hasLoaded: boolean =false;
+
+  onLoad(){
+    this.hasLoaded=true;
+  }
+
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt:string='';
+
+}
