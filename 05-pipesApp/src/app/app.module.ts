ee5429ccdd7464a79bd82cd95ad84f0d1cf19f6f
Bgpizarro <borjagarpiz@gmail.com>
2023-06-06 17:23:42 +0200

    150

---
+++ 05-pipesApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { SharedModule } from './shared/shared.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    SharedModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
